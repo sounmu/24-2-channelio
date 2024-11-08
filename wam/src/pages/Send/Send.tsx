b3610fdc8ada2be3f594c0bd688b4ea44f723cbc
nuegnues <13seun@gmail.com>
2024-11-09 01:35:38 +0900

    feat: summarize modal

--- wam/src/pages/Send/Send.tsx
+++ wam/src/pages/Send/Send.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useCallback } from 'react'
+import { useState, useEffect, useMemo, useCallback, ChangeEvent } from 'react'
 import {
   VStack,
   HStack,
@@ -32,6 +32,36 @@ function Send() {
   const chatType = useMemo(() => getWamData('chatType') ?? '', [])
   const broadcast = useMemo(() => Boolean(getWamData('broadcast') ?? false), [])
   const rootMessageId = useMemo(() => getWamData('rootMessageId'), [])
+
+  const [option, setOption] = useState<number>(0)
+  const [inputNum, setinputNum] = useState('')
+  const [selectedOption, setSelectedOption] = useState('오늘')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setinputNum(e.target.value)
+  }
+
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(e.target.value)
+  }
+
+  const getTimeStamp = useCallback(() => {
+    const millisecondsInADay = 24 * 60 * 60 * 1000 // 하루의 밀리초
+    const endValue = Date.now()
+    let startValue
+    if (selectedOption === '0') {
+      const now = new Date()
+      const startOfDay = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate()
+      ).getTime()
+      startValue = endValue - startOfDay
+    } else {
+      startValue = endValue - millisecondsInADay * Number(selectedOption)
+    }
+    return [startValue, endValue]
+  }, [selectedOption])
 
   const handleSend = useCallback(
     async (sender: string): Promise<void> => {
@@ -67,6 +97,27 @@ function Send() {
               },
             })
             break
+          case 'summarizeN':
+            await callFunction(appId, 'summarize', {
+              input: {
+                groupId: chatId,
+                personId: managerId,
+                flag: 0,
+                mesgNum: inputNum,
+              },
+            })
+            break
+          case 'summarizeD':
+            await callFunction(appId, 'summarize', {
+              input: {
+                groupId: chatId,
+                personId: managerId,
+                flag: 1,
+                startTime: getTimeStamp()[0],
+                endTime: getTimeStamp()[1],
+              },
+            })
+            break
           default:
             // NOTE: should not reach here
             console.error('Invalid message sender')
@@ -86,6 +137,8 @@ function Send() {
       managerId,
       message,
       rootMessageId,
+      inputNum,
+      getTimeStamp,
     ]
   )
 
@@ -97,7 +150,7 @@ function Send() {
           typo="24"
           bold
         >
-          Tutorial
+          Summarize
         </Text>
         <Button
           colorVariant="monochrome-dark"
@@ -106,37 +159,68 @@ function Send() {
           onClick={() => close()}
         />
       </HStack>
-      <HStack justify="center">
-        <ButtonGroup>
-          <Button
-            colorVariant="blue"
-            styleVariant="primary"
-            text="Send as a manager"
-            onClick={async () => {
-              await handleSend('manager')
-              close()
-            }}
+      {option === 0 ? (
+        <HStack justify="center">
+          <ButtonGroup>
+            <Button
+              colorVariant="blue"
+              styleVariant="primary"
+              text="최신 N개 메시지 요약"
+              onClick={() => setOption(1)}
+            />
+            <Button
+              colorVariant="blue"
+              styleVariant="primary"
+              text="시작 일시부터 지금까지 메시지 요약"
+              onClick={() => setOption(2)}
+            />
+          </ButtonGroup>
+        </HStack>
+      ) : option === 1 ? (
+        <HStack>
+          <input
+            type="number"
+            value={inputNum}
+            onChange={handleChange}
+            placeholder="숫자를 입력하세요"
           />
           <Button
             colorVariant="blue"
             styleVariant="primary"
-            text="Send as a bot"
+            text="요약"
             onClick={async () => {
-              await handleSend('bot')
+              await handleSend('summarizeN')
               close()
             }}
           />
+        </HStack>
+      ) : option === 2 ? (
+        <HStack>
+          <select
+            value={selectedOption}
+            onChange={handleSelectChange}
+          >
+            <option
+              value="0"
+              selected
+            >
+              오늘
+            </option>
+            <option value="3">3일</option>
+            <option value="5">5일</option>
+            <option value="7">7일</option>
+          </select>
           <Button
             colorVariant="blue"
             styleVariant="primary"
-            text="Send hello"
+            text="요약"
             onClick={async () => {
-              await handleSend('hello')
+              await handleSend('summarizeD')
               close()
             }}
           />
-        </ButtonGroup>
-      </HStack>
+        </HStack>
+      ) : null}
       <HStack justify="center">
         <Styled.CenterTextWrapper>
           <Icon
